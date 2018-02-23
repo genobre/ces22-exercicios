f49c048fa2b2f2e55249c1fc8a16a526d3275edc
Geovanna Nobre <geovannasnobre@gmail.com>
2018-02-23 16:00:38 -0300

    Adicionando Capítulo 4 - Exercício 9

---
+++ exercicio_4_9.py
@@ -0,0 +1,23 @@
+import turtle           #Importa a biblioteca que nos permite usar turtle
+
+#Função para desenhar estrela de 5 pontas, recebe como parâmetro uma tartaruga
+def make_star(t):
+    """Função que desenha estrela regular de 5 pontas, com o tamanho de cada lado
+    igual a 100. Recebe como parâmetro tartaruga t"""
+    t.hideturtle()
+    t.left(36)                  #Coloca tartaruga na posição inicial
+    size = 100                  #Define o lado da estrela como 100
+    for i in range(5):          #Loop para fazer a estrela
+        t.forward(size)
+        t.left(144)
+
+wn = turtle.Screen()                    #Cria e define características da janela
+wn.bgcolor("lightgreen")
+wn.title("Capítulo 4 - Exercício 9")
+
+alex = turtle.Turtle()                   #Cria e define características da tartaruga
+alex.pensize(4)
+
+make_star(alex)                         #Chama função para criar estrela
+
+wn.mainloop()           #Deixa a janela aberta
